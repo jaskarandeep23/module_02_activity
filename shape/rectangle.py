057791c96a4344b3cfcae91b6f9144a396743a0f
Jaskarandeep Kaur <jbhandol@rrc.ca>
2025-09-25 13:39:46 -0600

    Adding rectangle subclass with length/width validation and area/perimeter methods

--- shape/rectangle.py
+++ shape/rectangle.py
@@ -1,4 +1,34 @@
 """This module defines the Rectangle class."""
 
-__author__ = ""
-__version__ = ""
+__author__ = "Jaskarandeep kaur"
+__version__ = "25.09.25"
+
+from .shape import Shape
+
+class Rectangle(Shape):
+    """
+    Rectangle class with length and width in centimeters.
+    """
+
+    def __init__(self, color: str, length: int, width: int):
+        super().__init__(color)
+
+        if not isinstance(length, int):
+            raise ValueError("Length must be numeric.")
+        if not isinstance(width, int):
+            raise ValueError("Width must be numeric.")
+
+        self._length = length
+        self._width = width
+
+    def __str__(self) -> str:
+        base = super().__str__()
+        return (f"{base}\n"
+                f"This rectangle has four sides with the lengths of "
+                f"{self._length}, {self._width}, {self._length} and {self._width} centimeters.")
+
+    def calculate_area(self) -> int:
+        return self._length * self._width
+
+    def calculate_perimeter(self) -> int:
+        return 2 * (self._length + self._width)
